5f76037749c983d6e8430fd005890597a54f9122
VictorSMP <martinez.picardo.victor@gmail.com>
2016-02-25 18:51:39 +0100

    Primer commit, añadidos ejercicios + classTypeOf + stepThisNumber + divisors
    Signed-off-by: VictorSMP <martinez.picardo.victor@gmail.com>

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -2,6 +2,7 @@ package fp;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
@@ -11,8 +12,12 @@ public class Calculator {
      * este metodo devuelve el Class del object que le pasamos
      */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
-	}
+		return x.getClass();
+		/* Fuentes: 
+		 * https://msdn.microsoft.com/es-es/library/aa989117(v=vs.80).aspx
+		 * http://www.mundosica.com/2015/el-metodo-getclass-explicacion-y-uso-practico-construyendo-librerias-en-java-i/
+		 */
+	}	
 
 
 	/*
@@ -26,7 +31,17 @@ public class Calculator {
 	 * Escribir todos los números del number al 0 de step en step.
 	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
+		if(step==0)
+			return new int[0];
+		int length=(number-1)/step;
+		int stepping[]=new int[length];
+		int acum=0;
+		while(number>step){
+			number-=step;
+			stepping[acum]=number;
+			++acum;
+		}
+		return stepping;
 		}
 
 	/*
@@ -34,7 +49,25 @@ public class Calculator {
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		int length=0;
+		if(n>0){
+			for(int i=1; i<=n; i++)
+				if(n%i == 0)
+					length++;
+		}else return null;
+		int div[] = new int[length];
+		try{	
+			for (int i=n, j=0; i>0; i--) {
+				if(n%i == 0){
+					div[j] = i;
+					j++;
+				}
+			}
+			return div;
+		}catch(IndexOutOfBoundsException e){
+			return null;
+		}
+		
 	}
 
 	/*
